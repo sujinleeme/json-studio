59b57f817f25f80b9cdf8887e87e1ed46a52520f
sujinleeme <sujinlee.me@gmail.com>
2021-01-27 10:12:52 +0100

    add useToggle for checkbox

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -2,12 +2,10 @@ import React from "react";
 
 type CallbackType = (...args: string[]) => void;
 
-export const useToggle = (
-  initialValue = false
-): [value: boolean, toggle: () => void] => {
+export const useToggle = (initialValue = false): [boolean, () => void] => {
   const [value, setValue] = React.useState<boolean>(initialValue);
   const toggle = React.useCallback<CallbackType>(() => {
-    setValue((v) => !v);
+    setValue((v: boolean) => !v);
   }, []);
   return [value, toggle];
 };
