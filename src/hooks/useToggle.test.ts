73a53e57ff820115064ec81c4843571532543b82
sujinleeme <sujinlee.me@gmail.com>
2021-02-02 17:37:31 +0100

    add useToggle test

---
+++ src/hooks/useToggle.test.ts
@@ -0,0 +1,39 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+
+import { useToggle } from ".";
+
+test("should change the value to true if the initial value is empty and its first toggle is called.", () => {
+  const { result } = renderHook(() => useToggle());
+  const [value, toggle] = result.current;
+
+  // The default value is false
+  expect(value).toBe(false);
+
+  act(() => {
+    toggle();
+  });
+  expect(result.current[0]).toBe(true);
+
+  act(() => {
+    toggle();
+  });
+  expect(result.current[0]).toBe(false);
+});
+
+test("should change the value to true if the initial value has true and its first toggle is called.", () => {
+  const { result } = renderHook(() => useToggle(true));
+  const [value, toggle] = result.current;
+
+  // The default value is false
+  expect(value).toBe(true);
+
+  act(() => {
+    toggle();
+  });
+  expect(result.current[0]).toBe(false);
+
+  act(() => {
+    toggle();
+  });
+  expect(result.current[0]).toBe(true);
+});
