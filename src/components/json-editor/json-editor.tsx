e4f541647328571fa21aed3376ad1a816af7d490
sujinleeme <sujinlee.me@gmail.com>
2021-02-02 11:21:00 +0100

    remove paste event listener and fix validation condition

--- src/components/json-editor/json-editor.tsx
+++ src/components/json-editor/json-editor.tsx
@@ -34,37 +34,30 @@ export const JSONEditor: React.FC<JSONEditorProps> = ({
 
   const handleEditorWithoutPrettify = (value?: string) => setContent(value);
 
-  const handleEditorPrettify = (value: string) => {
-    const json = prettifyJsonString(value);
-    setContent(json);
-    // eslint-disable-next-line
-    const editor: any = editorRef.current;
-    // It might be a  @monaco-editor/react's problem.
-    // need to set new value inside. otherwise, it can't resize horizontal slider's  width.
-    if (!editor) return;
-    editor.setValue(json);
-  };
-
-  const handleEditorBeforeMount = () => {
-    if (defaultValue) {
-      handleEditorPrettify(defaultValue);
+  const handleEditorPrettify = (value?: string) => {
+    if (!value) {
+      setContent(undefined);
+    } else {
+      const json = prettifyJsonString(value);
+      setContent(json);
+      // eslint-disable-next-line
+      const editor: any = editorRef.current;
+      // It might be a  @monaco-editor/react's problem.
+      // need to set new value inside. otherwise, it can't resize horizontal slider's  width.
+      if (!editor) return;
+      editor.setValue(json);
     }
   };
+
+  const handleEditorBeforeMount = () => handleEditorPrettify(defaultValue);
 
   const handleEditorDidMount: OnMount = (editor) => {
     editorRef.current = editor;
     editor.getModel().updateOptions({ tabSize: 2, insertSpaces: false });
-    // eslint-disable-next-line
-    const editorEl = editor._domElement;
-    editorEl.addEventListener("paste", (event: ClipboardEvent) => {
-      // eslint-disable-next-line
-      const paste = (event.clipboardData || (window as any).clipboardData).getData("text");
-      handleEditorPrettify(paste);
-    });
   };
 
   const handleEditorChange = (value?: string) =>
-    value && isAutoPrettifyOn
+    isAutoPrettifyOn
       ? handleEditorPrettify(value)
       : handleEditorWithoutPrettify(value);
 
@@ -74,19 +67,20 @@ export const JSONEditor: React.FC<JSONEditorProps> = ({
     const errorMessage = markers.map(
       ({ startLineNumber, message }) => `line ${startLineNumber}: ${message}`
     );
-    setIsValidJson(!(errorMessage.length > 0));
+    const hasContent: boolean = !!content && content?.length > 0;
+    const hasError: boolean = errorMessage.length > 0;
+    setIsValidJson(hasContent && !hasError);
     setErrors(errorMessage);
   };
 
   const handleMinifyClick = () => {
-    const minifyJson = content && minifyJsonString(content);
+    const minifyJson: string | undefined = content
+      ? minifyJsonString(content)
+      : content;
     handleEditorWithoutPrettify(minifyJson);
   };
 
-  const handlePrettifyClick = () => {
-    if (!content) return;
-    handleEditorPrettify(content);
-  };
+  const handlePrettifyClick = () => handleEditorPrettify(content);
 
   const handleUploadClick = (file: File) => {
     const fileReader = new FileReader();
