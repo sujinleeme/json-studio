ba2489ff11e6b016bf37d2d3dce41d31f4d592ec
sujinleeme <sujinlee.me@gmail.com>
2021-02-06 23:37:25 +0100

    change vars naming

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -3,9 +3,9 @@ import React from "react";
 type CallbackType = (...args: string[]) => void;
 
 export const useToggle = (initialValue = false): [boolean, () => void] => {
-  const [value, setValue] = React.useState<boolean>(initialValue);
+  const [toggled, setToggled] = React.useState<boolean>(initialValue);
   const toggle = React.useCallback<CallbackType>(() => {
-    setValue((v: boolean) => !v);
+    setToggled((v) => !v);
   }, []);
-  return [value, toggle];
+  return [toggled, toggle];
 };
