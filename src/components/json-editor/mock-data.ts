767d3a4e420145787392206ee34685d73af994f7
sujinleeme <sujinlee.me@gmail.com>
2021-02-14 23:10:47 +0100

    add mock data

---
+++ src/components/json-editor/mock-data.ts
@@ -0,0 +1,9 @@
+export const jsonInput =
+  '{"name": "Lucrezia Nethersole", "email": "l.nethersole@hotmail.com", "date_of_birth": "2007–01–23" }';
+
+export const schema = `{ "name": "sample schema", "type": "object", "properties": { "name": { "title": "Name", "type": "string", "description": "Users full name supporting unicode but no emojis.", "maxLength": 20 }, "email": { "title": "Email", "description": "Like a postal address but for computers.", "type": "string", "format": "email" }, "date_of_birth": { "title": "Date Of Birth", "type": "string", "description": "Date of uses birth in the one and only date standard: ISO 8601.", "format": "date", "example": "1990–12–28" } }, "required": [ "name" ] }`;
+
+export const mockData = {
+  jsonInput,
+  schema,
+};
