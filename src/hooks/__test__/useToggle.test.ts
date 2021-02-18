855d33ab383de5c24dbee6a174656ce19be29abc
sujinleeme <sujinlee.me@gmail.com>
2021-02-18 09:44:10 +0100

    fix useToggle test

--- src/hooks/__test__/useToggle.test.ts
+++ src/hooks/__test__/useToggle.test.ts
@@ -1,14 +1,15 @@
 import { act, renderHook } from "@testing-library/react-hooks";
 
-import { useToggle } from "..";
+import { useToggle } from "../useToggle";
 
 describe("useToggle", () => {
-  test("should change the value to true if the initial value is empty and its first toggle is called.", () => {
+  test("should change the value to false if the initial value is empty and its first toggle is called.", () => {
     const { result } = renderHook(() => useToggle());
-    const [value, toggle] = result.current;
+    const [toggled, toggle] = result.current;
 
     // The default value is false
-    expect(value).toBe(false);
+    expect(toggled).toBe(false);
+    expect(result.current[0]).toBe(false);
 
     act(() => {
       toggle();
